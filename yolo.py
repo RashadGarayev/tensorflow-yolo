c0f49ffbca3c2bae0162a288ddc329227dab739d
Rashad Garayev <pythonaz@yahoo.com>
2019-03-15 11:07:32 +0400

    Update yolo.py

--- yolo.py
+++ yolo.py
@@ -5,7 +5,7 @@ import cv2
 from darkflow.net.build import TFNet
 
 config = tf.ConfigProto(log_device_placement=True)
-#config.gpu_option.allow_growth=True
+config.gpu_option.allow_growth=True
 with tf.Session(config=config) as sess:
     options = {
         'model' : 'cfg/yolo.cfg',
@@ -15,7 +15,7 @@ with tf.Session(config=config) as sess:
     }
     tfnet=TFNet(options)
 colors=[tuple(255*np.random.rand(3)) for _ in range(10)]
-capture=cv2.VideoCapture('B.mp4')
+capture=cv2.VideoCapture(0)
 #capture.set(cv2.CAP_PROP_FRAME_WIDTH,1920)
 #capture.set(cv2.CAP_PROP_FRAME_HEIGHT,1080)
 
@@ -33,7 +33,7 @@ while True:
             frame=cv2.rectangle(frame,tl,br,color,5)
             frame=cv2.putText(frame,text,tl,cv2.FONT_ITALIC,1,(0,0,0),2)
         cv2.imshow('frame',frame)
-        #print('FPS {:.1f}'.format(1/(time.time()-stime)))
+        print('FPS {:.1f}'.format(1/(time.time()-stime)))
 		
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
